72f82cce8ec4a79476d6caa5b55b11079e74ca33
encrypthink <wijay11ipa@gmail.com>
2022-09-09 07:16:39 +0700

    fixin grammar migration

--- modules/databases/migration.py
+++ modules/databases/migration.py
@@ -1,32 +1,40 @@
 class Schema:
-    query = ""
+    fields = []
+    def create(self, table_name, grammar):
+        for i in grammar:
+            self.fields.append(str(i))
+        
+        print("CREATE TABLE {} ({})".format(table_name, ", ".join(self.fields)))
 
-    
-    def create(self, grammar):
-        print("CREATE TABLE users ({})".format(grammar))
 
 class Blueprint(Schema):
+    query = ""
+
     def id(self):
-        self.query += "id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY"
+        self.query = "id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY"
         return self
 
     def string(self, name, length=255):
-        self.query += "{} VARCHAR({})".format(name, length)
+        self.query = "{} VARCHAR({})".format(name, length)
         return self
 
     def integer(self, name, length=5):
-        self.query += "{} INT({})".format(name, length)
+        self.query = "{} INT({})".format(name, length)
         return self
 
     def not_null(self):
-        self.query += "{} NOT NULL".format(self.query)
+        self.query = "{} NOT NULL".format(self.query)
         return self
 
-class Test(Blueprint):
+    def __str__(self):
+        return self.query
+
+
+class Test:
     def up(self):
-        Schema().create([
-            self.string("username").not_null(),
-            self.string("password").not_null(),
+        Schema().create("users", [
+            Blueprint().string("username").not_null(),
+            Blueprint().string("password"),
         ])
 
 t = Test()

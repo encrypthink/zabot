17b78f4bf6100e705fb0ddcd0754db8c2e02d2a0
codegraphs11 <wijay11ipa@gmail.com>
2022-09-12 23:47:47 +0700

    simply logic at migration first step

--- modules/commander/src/actions/runner.py
+++ modules/commander/src/actions/runner.py
@@ -1,6 +1,5 @@
 from asyncore import read
 from cgi import print_directory
-import collections
 from modules.databases.database import Database
 from modules.helpers.finder import Finder
 import importlib.util
@@ -17,22 +16,18 @@ class Runner:
         all_migrations = Finder(migration_directory).get_directory_list()
 
         if len(exist_table) < 1:
-            i = 0
             
             for migration in all_migrations:
-                if "create_migrations_table" in all_migrations[i]["filename"]:
-                    self.__execute_migration(all_migrations[i]["filename"], all_migrations[i]["fullpath"])
+                if "create_migrations_table" in all_migrations[migration]["filename"]:
+                    self.__execute_migration(all_migrations[migration]["filename"], all_migrations[migration]["fullpath"])
                     new_migration_list = dict(all_migrations)
-                    del new_migration_list[i]
+                    del new_migration_list[migration]
                     all_migrations = new_migration_list
-                i = i+1
 
-            i = 1
             for migration in all_migrations:
-                self.__execute_migration(all_migrations[i]["filename"], all_migrations[i]["fullpath"])
-                connection.syntax_execution("INSERT INTO migrations(migration, steps) VALUES ('{}', {})".format(all_migrations[i]["filename"], 1))
+                self.__execute_migration(all_migrations[migration]["filename"], all_migrations[migration]["fullpath"])
+                connection.syntax_execution("INSERT INTO migrations(migration, steps) VALUES ('{}', {})".format(all_migrations[migration]["filename"], 1))
 
-                i = i+1
         else:
             already_migrate = connection.fetch_all("SELECT migration FROM migrations")
             steps = connection.fetch_one("SELECT max(steps) from migrations")[0] + 1
@@ -62,18 +57,6 @@ class Runner:
             else:
                 print("Nothing migrated")
 
-            
-            """
-            i = 0
-            for migration in all_migrations:
-                for migrated in migrated_list:
-                    print(all_migrations[i]["filename"])
-                    #     if migrated not in all_migrations[i]["filename"]:
-                    #         will_be_migrated.append(all_migrations[i]["filename"])
-                            # self.__execute_migration(all_migrations[i]["filename"], all_migrations[i]["fullpath"])
-                            # connection.syntax_execution("INSERT INTO migrations(migration, steps) VALUES ('{}', {})".format(all_migrations[i]["filename"], steps))
-                i = i+1
-            """
                 
     def __execute_migration(self, filename, filepath):
         spec = importlib.util.spec_from_file_location(filename, filepath)
